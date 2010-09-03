2567838bd3b29f1cd68b43c8c05144f9a1397f84
andy <andrerlind@gmail.com>
2010-09-02 22:48:40 -0500

    version without save

--- datamodel.py
+++ datamodel.py
@@ -3,5 +3,6 @@ from google.appengine.ext import db
 class Skill(db.Model):
     user = db.UserProperty()
     name = db.StringProperty()
+    category = db.StringProperty()
     proficiency = db.StringProperty()
     dateUpdated = db.DateTimeProperty(auto_now_add=True)
--- skills-tracker.py
+++ skills-tracker.py
@@ -8,6 +8,32 @@ from google.appengine.ext.webapp import template
 from datamodel import Skill
 
 class MainPage(webapp.RequestHandler):
+    def get(self): 
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'        
+        user = users.get_current_user()
+        userNickname = user.nickname()
+        
+        recentSkillsQuery =  db.GqlQuery("SELECT * FROM Skill " +
+                "WHERE user = :1 " +
+                "ORDER BY dateUpdated DESC",
+                user)
+        recentSkills = recentSkillsQuery.fetch(10)
+        
+        template_values = {
+            'recentSkills': recentSkills,
+            'user': userNickname,
+            'url': url,
+            'url_linktext': url_linktext
+        }
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+class SkillsUpdater(webapp.RequestHandler):
     def get(self):    
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
@@ -15,18 +41,44 @@ class MainPage(webapp.RequestHandler):
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'        
-        user = users.get_current_user().nickname()
+        user = users.get_current_user()
+        userNickname = user.nickname()
         
-        skillsQuery = Skill.all().order('-dateUpdated')
-        currentSkills = skillsQuery.fetch(100)
+        uiSkillsQuery =  db.GqlQuery("SELECT * FROM Skill " +
+                "WHERE category = :1 AND user = :2 " +
+                "ORDER BY name DESC",
+                "UI", user)
+        uiSkills = uiSkillsQuery.fetch(100)
+                
+        javaSkillsQuery =  db.GqlQuery("SELECT * FROM Skill " +
+                "WHERE category = :1 AND user = :2 " +
+                "ORDER BY name DESC",
+                "Java", user)
+
+        javaSkills = javaSkillsQuery.fetch(100)
+                
+        dotNetSkillsQuery =  db.GqlQuery("SELECT * FROM Skill " +
+                "WHERE category = :1 AND user = :2 " +
+                "ORDER BY name DESC",
+                ".NET", user)
+        dotNetSkills = dotNetSkillsQuery.fetch(100)
+                
+        toolsSkillsQuery =  db.GqlQuery("SELECT * FROM Skill " +
+                "WHERE category = :1 AND user = :2 " +
+                "ORDER BY name DESC",
+                "Tools", user)
+        toolsSkills = toolsSkillsQuery.fetch(100)
         
         template_values = {
-            'currentSkills': currentSkills,
-            'user': user,
+            'uiSkills': uiSkills,
+            'javaSkills': javaSkills,
+            'dotNetSkills': dotNetSkills,
+            'toolsSkills': toolsSkills,
+            'user': userNickname,
             'url': url,
             'url_linktext': url_linktext
             }
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        path = os.path.join(os.path.dirname(__file__), 'update.html')
         self.response.out.write(template.render(path, template_values))
 
 class AddSkill(webapp.RequestHandler):
@@ -38,8 +90,20 @@ class AddSkill(webapp.RequestHandler):
         newSkill.put()
         self.redirect('/')
 
+class Setup(webapp.RequestHandler):
+    def get(self):
+        newSkill = Skill()
+        newSkill.name =  'Java'
+        newSkill.category =  'Java'
+        newSkill.proficiency = '3'
+        newSkill.user = users.get_current_user()
+        newSkill.put()
+        self.redirect('/')
+
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
+                                      ('/update', SkillsUpdater),
+                                      ('/setup', Setup),
                                       ('/AddSkill', AddSkill)],
                                      debug=True)
 
